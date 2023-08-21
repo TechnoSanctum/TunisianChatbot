05cc25874553704f631b3e17834e2efd73c454bc
TechnoSanctum <72281194+TechnoSanctum@users.noreply.github.com>
2023-08-22 00:40:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request
-from chatterbot import ChatBot
+
 app = Flask(__name__)
+'''
 last_message = ""
 rp = 0
 TunisianBot = ChatBot("Tunisianbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
-
+'''
 
 @app.route("/")
 def index():
@@ -12,23 +13,8 @@ def index():
 
 @app.route("/get")
 def get_bot_response():
-    global last_message
-    global rp
+    
     userText = request.args.get('msg')
-    if userText == "":
-        return "..."
-    if userText == last_message:
-        rp += 1
-        if rp == 3:
-            return "3lah t3awed fi nafs jomla?"
-        elif rp>3:
-            return ''
-            
-        return "Najem n3awnek fi 7aja?"
-    else:
-        rp = 0
-        last_message = userText
-        return str(TunisianBot.get_response(userText))
-
+    return userText
 if __name__ == "__main__":
     app.run()
